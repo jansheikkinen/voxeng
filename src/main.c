f7e0a1fb8b2e3ca537b0f13194391ac38e7b7c06
jansheikkinen <jansheikkinen@mail.com>
2022-08-10 22:10:52 -0500

    Rendering world and chunks; big stone cube :)

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 /* main.c */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #include <math.h>
 #include <raylib.h>
@@ -12,7 +13,7 @@ const size_t scrHeight = 500;
 
 // The size of a voxel, number of voxels in a chunk,
 // and number of chunks in the world
-const size_t voxelSize = 2;
+const size_t voxelSize = 1; // TODO: !1 sizes still kinda funky
 const size_t chunkSize = 4;
 const size_t worldSize = 2;
 
@@ -22,8 +23,8 @@ enum VoxelType {
   VOXEL_DIRT  = 1,
   VOXEL_STONE = 2,
 };
-//
-  // Placehold voxel colours
+
+// Placehold voxel colours
 const Color voxelColors[] = {
   {   0,   0,   0,   0 },
   { 140,  80,  30, 255 },
@@ -47,6 +48,78 @@ struct World {
   struct Chunk** chunks;
 };
 
+size_t posToIndex(Vector3 pos, int mult) {
+  return (pos.z * mult * mult) + (pos.y * mult) + pos.x;
+}
+
+void initializeChunk(struct Chunk* chunk, Vector3 position) {
+  chunk->voxels = calloc(pow(chunkSize, 3), sizeof(*chunk->voxels));
+  chunk->position = position;
+
+  for(size_t z = 0; z < chunkSize; z++) {
+    for(size_t y = 0; y < chunkSize; y++) {
+      for(size_t x = 0; x < chunkSize; x++) {
+        float voxelSizeHalf = voxelSize / 2.0;
+
+        chunk->voxels[posToIndex((Vector3){ x, y, z }, chunkSize)] =
+          (struct Voxel){
+            (Vector3){
+              (chunkSize * position.x) + (x * voxelSize) + voxelSizeHalf,
+              (chunkSize * position.y) + (y * voxelSize) + voxelSizeHalf,
+              (chunkSize * position.z) + (z * voxelSize) + voxelSizeHalf,
+            },
+           VOXEL_STONE
+        };
+      }
+    }
+  }
+}
+
+void initializeWorld(struct World* world) {
+  world->chunks = calloc(pow(worldSize, 3), sizeof(struct Chunk*));
+
+  for(size_t z = 0; z < worldSize; z++) {
+    for(size_t y = 0; y < worldSize; y++) {
+      for(size_t x = 0; x < worldSize; x++) {
+        Vector3 pos = { x, y, z };
+
+        world->chunks[posToIndex(pos, worldSize)] =
+          malloc(sizeof(struct Chunk));
+
+        initializeChunk(world->chunks[posToIndex(pos, worldSize)], pos);
+      }
+    }
+  }
+}
+
+void destroyChunk(struct Chunk* chunk) {
+  free(chunk->voxels);
+}
+
+void destroyWorld(struct World* world) {
+  for(size_t z = 0; z < worldSize; z++) {
+    for(size_t y = 0; y < worldSize; y++) {
+      for(size_t x = 0; x < worldSize; x++) {
+        destroyChunk(world->chunks[posToIndex(
+              (Vector3){ x, y, z },
+              worldSize)]);
+      }
+    }
+  }
+}
+
+void renderVoxel(struct Voxel voxel) {
+  DrawCube(
+      voxel.position,
+      voxelSize, voxelSize, voxelSize,
+      voxelColors[voxel.type]);
+
+  DrawCubeWires(
+      voxel.position,
+      voxelSize, voxelSize, voxelSize,
+      BLACK);
+}
+
 
 int main(void) {
   // Initialise the window
@@ -63,6 +136,8 @@ int main(void) {
   // You can pan with middle mouse
   // and rotate by holding alt and doing middle mouse
 
+  struct World world = { 0 };
+  initializeWorld(&world);
 
   // Game loop
   while(!WindowShouldClose()) {
@@ -78,15 +153,14 @@ int main(void) {
     BeginMode3D(camera);
 
     // Render the world
-    DrawCube((Vector3){ 1, 1, 1 },
-        voxelSize, voxelSize, voxelSize,
-        voxelColors[VOXEL_DIRT]);
-    DrawCubeWires((Vector3){ 1, 1, 1 },
-          voxelSize, voxelSize, voxelSize,
-          BLACK);
+    for(size_t i = 0; i < pow(worldSize, 3); i++) {
+      for(size_t j = 0; j < pow(chunkSize, 3); j++) {
+        renderVoxel(world.chunks[i]->voxels[j]);
+      }
+    }
 
     // Draw grids to show voxel, chunk, and world sizes
-    DrawGrid(chunkSize * voxelSize, voxelSize);
+    DrawGrid(2 * chunkSize * voxelSize, voxelSize);
     DrawGrid(worldSize * chunkSize, chunkSize);
 
     EndMode3D();
@@ -100,5 +174,7 @@ int main(void) {
 
   // Actually close the window
   CloseWindow();
+
+  destroyWorld(&world);
   return 0;
 }
