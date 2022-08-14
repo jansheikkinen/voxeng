a39706f7cf5c7daf2c0081345ac55f97d66495df
Hayden <asianhaydenxd@gmail.com>
2022-08-14 02:10:09 -0400

    Remove testing prints

--- src/main.c
+++ src/main.c
@@ -56,7 +56,6 @@ int main(void) {
   // You can pan with middle mouse
   // and rotate by holding alt and doing middle mouse
 
-  printf("test\n\n");
   // Game loop
   while(!WindowShouldClose()) {
     // Game logic
