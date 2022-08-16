5ff8ffb79c970c102ef25b74e677b052b5c0fb8a
Hayden <asianhaydenxd@gmail.com>
2022-08-16 03:48:17 -0400

    Implement uintPow util function

--- src/voxel/voxel.c
+++ src/voxel/voxel.c
@@ -9,3 +9,11 @@
 size_t posToIndex(int x, int y, int z, int mult) {
   return abs((z * mult * mult) + (y * mult) + x);
 }
+
+size_t uintPow(size_t base, size_t exponent) {
+  size_t n = 1;
+  for(register size_t i = 0; i < exponent; i++) {
+    n *= base;
+  }
+  return n;
+}
